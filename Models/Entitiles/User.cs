96cbefef7e178e81664cfb23f27b7b67cfdd9ab6
Joshua <joshuapavan35@gmail.com>
2025-07-29 23:46:10 +0530

    Adds migration to modify the user models ID

--- Models/Entitiles/User.cs
+++ Models/Entitiles/User.cs
@@ -4,7 +4,7 @@ namespace Grandeur_BE_DotNet.Models.Entitiles;
 
 public class User
 {
-    public Guid Id { get; set; }
+    public int Id { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public required string Email { get; set; }
