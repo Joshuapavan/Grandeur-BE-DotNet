33708a66ae0dca6e57a7ee5a8d2c0143c3e169c7
Joshua <joshuapavan35@gmail.com>
2025-08-01 10:06:52 +0530

    Adds Car DTO

---
+++ DTO/CarRequestDto.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Grandeur_BE_DotNet.DTO;
+
+public class CarRequestDto
+{
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+    public required string Model { get; set; }
+    public required string CarNumber { get; set; }
+    public required string YearOfManufacture { get; set; }
+    public required string KmsDriven { get; set; }
+    public required string Price { get; set; }
+    public required string AnyDamages { get; set; }
+    public required string NoOfOwners { get; set; }
+    public bool Insured { get; set; }
+}
